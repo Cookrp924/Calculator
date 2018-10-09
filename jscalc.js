ab891116f75516e3475d18300764520e4719ac34
Cookrp924 <34467782+Cookrp924@users.noreply.github.com>
2018-10-09 06:35:19 -0400

    Update jscalc.js

--- jscalc.js
+++ jscalc.js
@@ -1,15 +1,20 @@
 var result = 0;
-var resultText = document.getElementById("result");
-var eqText = document.getElementById("equation")
+var resultText = document.getElementById("result"); //main #s shown
+var eqText = document.getElementById("equation") //the equation down below
 var numbers = document.getElementsByClassName("num");
 var exec = document.getElementsByClassName("exec");
-var execFunc = "";
+var neg = document.getElementById("neg");
+var negSwitch = 0; //used for switching b/t pos & neg numbers
+var switchNum = 1; //differentiates b/t num1 & num2 when using +/-
+var dec = document.getElementById("dec");
+var execFunc = ""; //the chosen operator
 var num1 = [];
 var num2 = [];
 var equals = document.getElementById("equals");
 var allClear = document.getElementById("allClear");
 var clrEntry = document.getElementById("clrEntry");
 
+//clear all on calc
 allClear.addEventListener("click", function(){
 	execFunc = "";
 	num1 = [];
@@ -17,27 +22,29 @@ allClear.addEventListener("click", function(){
 	result = 0;
 	resultText.textContent = 0;
 	eqText.textContent = 0
+	dec.style.pointerEvents = "auto";
 	console.log("cleared " + num1 + " " + num2);
 })
 
+//clear entry
 clrEntry.addEventListener("click", function(){
-	//execFunc = "";
+	//if operation has not been chosen (no num2, num1 array not yet joined)
 	if(num1.length > 0){
 		num1 = [];
 		result = 0;
 		resultText.textContent = 0;
 		eqText.textContent = 0;
 	}
+	//if operation has been chosen, but num2 array not yet joined
 	if(num1.length == null){
 		num2 = [];
 		resultText.textContent = num1;
 		eqText.textContent = num1 + " " + execFunc;
 	}
-	//num2 = [];
-	//resultText.textContent = 0
 	console.log("cleared entry");
 })
 
+//each number key press
 for(i = 0; i < numbers.length; i++){
 	numbers[i].addEventListener("click", function(){
 		if(execFunc === ""){
@@ -54,25 +61,33 @@ for(i = 0; i < numbers.length; i++){
 	})
 }
 
+//selecting an operator 
 for(i = 0; i < exec.length; i++){
 	exec[i].addEventListener("click", function(){
 		execFunc = this.value;
 		console.log(this.value);
 		if(num1.length > 0){
 			num1 = Number(num1.join(""));
+			switchNum++;
+			console.log(switchNum);
 			console.log(num1);
 		}else if(num1.length === 0){
 			num1 = 0;
+			switchNum++;
+			console.log(switchNum);
 			console.log(num1);
 		}
 		if(num2.length > 0){
 			num2 = Number(num2.join(""));
+			switchNum--;
+			console.log(switchNum);
 			operator();
 		}
 		eqText.textContent = num1 + " " + execFunc;
 	})
 }
 
+//'equals' num2 is joined and operator function runs
 equals.addEventListener("click", function(){
 	if(num2.length > 0){
 		num2 = Number(num2.join(""));
@@ -81,6 +96,7 @@ equals.addEventListener("click", function(){
 	operator();
 })
 
+//switch statements performs equation based on chosen operator
 function operator(){
 	switch(execFunc){
 		case "+":
@@ -112,6 +128,71 @@ function operator(){
 	}
 	num1 = result;
 	num2 = [];
+}
+
+//disable decimal btn after click; re-enable after operator is chosen
+dec.addEventListener("click", function(){
+	dec.style.pointerEvents = "none";
+})
+
+for(i = 0; i < exec.length; i++){
+	exec[i].addEventListener("click", function(){
+		dec.style.pointerEvents = "auto";
+	})
+}
+
+//negative number
+neg.addEventListener("click", function(){
+	negPos();
+})
+
+//allows user to switch current # from pos. to neg. seamlessly
+//if switchNum = 1, num1 is being manipulated; if switchNum = 2, then num2
+//negSwitch assigned a value denoting positive(0) or negative(1) value
+function negPos(){
+	switch(switchNum){
+		//num1 being manipulated
+		case 1:
+			if(negSwitch === 0){
+				negSwitch++; //negSwitch will equal 1
+				console.log("negSwitch=" + negSwitch);
+				num1.splice(0, 0, "-");
+				resultText.textContent = num1.join("");
+				eqText.textContent = num1.join("");
+			}else{
+				negSwitch--; //negSwitch will equal 0
+				console.log("negSwitch=" + negSwitch);
+				num1.shift();
+				if(num1.length > 0){
+					resultText.textContent = num1.join("");
+					eqText.textContent = num1.join("");
+				}else{
+					resultText.textContent = 0;
+					eqText.textContent = 0;
+				}
+			}
+			break;
+		case 2:
+			if(negSwitch === 0){
+				negSwitch++;
+				console.log("negSwitch=" + negSwitch);
+				num2.splice(0, 0, "-");
+				resultText.textContent = num2.join("");
+				eqText.textContent = num1 + " " + execFunc + " " + num2.join("");
+			}else{
+				negSwitch--;
+				console.log("negSwitch=" + negSwitch);
+				num2.shift();
+				if(num2.length > 0){
+					resultText.textContent = num2.join("");
+					eqText.textContent = num1 + " " + execFunc;
+				}else{
+					resultText.textContent = num1;
+					eqText.textContent = num1 + " " + execFunc;
+				}
+			}
+			break;
+	}
 }
 
 
